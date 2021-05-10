027c0f4b9ec760620963dce4772b75c574a4ca21
Bryan Montoya Osorio <42284607+bryanMontoya@users.noreply.github.com>
2021-05-09 22:50:20 -0500

    Codigo para leer peso de celda de carga y publicarlo en tópico MQTT.

---
+++ DataCollection/src/Credentials.h
@@ -0,0 +1,22 @@
+#ifndef ARDUINO_CREDENTIALS_H
+#define ARDUINO_CREDENTIALS_H
+
+/* WiFi Credentials to connect Internet */
+#define STA_SSID "xxxx"
+#define STA_PASS "xxxx"
+
+/* Provide MQTT broker credentials as denoted in maqiatto.com. */
+#define MQTT_BROKER       "xxxx"
+#define MQTT_BROKER_PORT  1883
+#define MQTT_USERNAME     "xxxx"
+#define MQTT_KEY          "xxxx"
+
+
+/* Provide topic as it is denoted in your topic list. 
+ * For example mine is : cadominna@gmail.com/topic1
+ * To add topics, see https://www.maqiatto.com/configure
+ */
+#define TOPICPESODIAMANTES      "xxxx"
+#define TOPICPESOORO            "xxxx"
+
+#endif /* ARDUINO_CREDENTIALS_H */
---
+++ DataCollection/src/main.cpp
@@ -0,0 +1,82 @@
+#include <ESP8266WiFi.h>
+#include <Arduino.h>
+#include "HX711.h"
+
+#include "mqtt/MQTTConnector.h"
+#include "Credentials.h"
+
+HX711 balanza;
+const int DOUT = 16;
+const int CLK = 5;
+
+double Peso;
+
+double sensarPeso(){
+  double Peso;    
+
+  delay(2000);
+  Serial.print("Peso: ");
+  Peso = balanza.get_units(20);
+  Peso = Peso*(-1);
+  Serial.print(Peso);
+  Serial.println(" gramos.");  
+  return Peso;
+}
+
+void calibracion(){  
+  //Solo es necesario ejecutar cuando se debe realizar la calibración de la balanza.
+    balanza.begin(DOUT, CLK);
+    Serial.print("Lectura del valor del ADC:");
+    Serial.println(balanza.read());
+    Serial.println("No ponga ningún objeto sobre la balanza.");
+    Serial.println("Destarando...");
+    balanza.set_scale(); //La escala por defecto es 1
+    balanza.tare(20);  //El peso actual es considerado Tara.
+    Serial.println("Coloque un peso conocido:");
+
+  while(1){
+    Serial.print("Valor de lectura: ");
+    Serial.println(balanza.get_value(10),0);
+    delay(100);
+  }  
+}
+
+void WiFiBegin(const char* ssid, const char* pass)
+{
+  WiFi.begin(ssid, pass);
+  Serial.printf("Waiting for AP connection ...\n");
+  while(WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.printf(".");
+  }
+  IPAddress ip = WiFi.localIP();
+  Serial.printf("IP : %d.%d.%d.%d\n", ip[0],ip[1],ip[2],ip[3]);
+}
+
+void setup() 
+{
+  Serial.begin(115200);  
+  WiFiBegin(STA_SSID, STA_PASS);
+  MQTTBegin();  
+
+  balanza.begin(DOUT, CLK);
+  Serial.print("Lectura del valor del ADC:  ");
+  Serial.println(balanza.read());
+  Serial.println("No ponga ningun  objeto sobre la balanza");
+  Serial.println("Destarando...");
+  Serial.println("...");
+  // Establecer la escala.
+  balanza.set_scale(409); 
+  balanza.tare(20);       //El peso actual es considerado Tara.  
+  Serial.println("Listo para pesar");  
+}
+
+void loop() 
+{  
+  MQTTLoop(); 
+  //Pubica el peso sensado en tópico MQTT.  
+  Peso = sensarPeso();
+  String output = "Peso: " + String( Peso ) + " gramos.";  
+  MQTTPublish(TOPICPESOORO, (char *)output.c_str());  
+}
