e757ed7252a90c598e24c34dfb97d526e78d7775
Bryan Montoya Osorio <42284607+bryanMontoya@users.noreply.github.com>
2021-06-02 20:48:05 -0500

    Se añade time stamp para la gramera.

--- DataCollection/src/main.cpp
+++ DataCollection/src/main.cpp
@@ -2,44 +2,24 @@
 #include <Arduino.h>
 #include "HX711.h"
 #include <math.h>
-
 #include "mqtt/MQTTConnector.h"
+#include "rest/Rest.h"
 #include "Credentials.h"
 
 HX711 balanza;
 const int DOUT = 16;
 const int CLK = 5;
-
 double Peso;
 
 double sensarPeso(){
   double Peso;    
-
   delay(3000);
   Serial.print("Peso: ");
   Peso = balanza.get_units(20);
-  Peso = Peso*(-1);
+  Peso = Peso*(1); //*(-1) en caso de poner la balanza al revés y estar recibiendo valores negativos.
   Serial.print(Peso);
   Serial.println(" gramos.");  
   return Peso;
-}
-
-void calibracion(){  
-  //Solo es necesario ejecutar cuando se debe realizar la calibración de la balanza.
-    balanza.begin(DOUT, CLK);
-    Serial.print("Lectura del valor del ADC:");
-    Serial.println(balanza.read());
-    Serial.println("No ponga ningún objeto sobre la balanza.");
-    Serial.println("Destarando...");
-    balanza.set_scale(); //La escala por defecto es 1
-    balanza.tare(20);  //El peso actual es considerado Tara.
-    Serial.println("Coloque un peso conocido:");
-
-  while(1){
-    Serial.print("Valor de lectura: ");
-    Serial.println(balanza.get_value(10),0);
-    delay(100);
-  }  
 }
 
 void WiFiBegin(const char* ssid, const char* pass)
@@ -60,24 +40,24 @@ void setup()
   Serial.begin(115200);  
   WiFiBegin(STA_SSID, STA_PASS);
   MQTTBegin();  
-
   balanza.begin(DOUT, CLK);
   Serial.print("Lectura del valor del ADC:  ");
   Serial.println(balanza.read());
   Serial.println("No ponga ningun  objeto sobre la balanza");
   Serial.println("Destarando...");
-  Serial.println("...");
-  // Establecer la escala.
-  balanza.set_scale(406.3); 
-  balanza.tare(20);       //El peso actual es considerado Tara.  
+  Serial.println("...");  
+  balanza.set_scale(406.3);  // Establecer la escala.
+  balanza.tare(20);       
   Serial.println("Listo para pesar");  
 }
 
 void loop() 
-{  
+{      
   MQTTLoop(); 
+  //Llamada a api.
+  String respuestaApi = apiRest("http://worldtimeapi.org/api/timezone/America/Bogota");  
+  Peso = lround(sensarPeso()); //Aproximar peso al valor más cercano.    
   //Pubica el peso sensado en tópico MQTT.  
-  Peso = lround(sensarPeso()); //Aproximar al más cercano.
-  String output = "{ \"item\" : \"diamantes\", \"weight\": " + String( Peso ) + "}";  
+  String output = "{ \"item\" : \"diamantes\",\"weight\" : " + String( Peso ) + "," + " \"date\" : " + respuestaApi.substring(0, 10) + "," + "\"Hour\" : " + respuestaApi.substring(11, 16) + "}";
   MQTTPublish(TOPIC, (char *)output.c_str());  
 }
